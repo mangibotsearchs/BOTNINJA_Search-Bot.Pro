54235473bf20d8af372eab7ab7acf1bfb8149309
mangibotsearchs <131441149+mangibotsearchs@users.noreply.github.com>
2023-04-29 22:41:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/mangibotsearchs/BOTNINJA_Search-Bot.Pro /BOTNINJA_Search-Bot.Pro
+cd /BOTNINJA_Search-Bot.Pro
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
